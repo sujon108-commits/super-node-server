dff5fb8865d80a64125a183f213115020e2c741b
Pj <pj@gmail.com>
2024-02-11 18:54:55 +0530

    Fixed market data

--- src/sockets/OddSocket.ts
+++ src/sockets/OddSocket.ts
@@ -4,7 +4,8 @@ import MatchController from "../controllers/api/MatchController";
 import rethink from "../database/rethinkdb";
 import { tables } from "../utils/rethink-tables";
 import Websocket from "./Socket";
-
+import { IMarket, IRunnerType } from "../models/MarketModel";
+const checkOddsLength = 3;
 class OddSocket {
   io: any;
   constructor(private socket: Socket) {
@@ -24,9 +25,10 @@ class OddSocket {
       const markets = await marketsPromise.toArray();
       if (markets.length > 0) {
         console.log("join room", markets);
-        markets.map((market: any) => {
+        markets.map((market: IMarket) => {
+          const marketData = this.convertDataToMarket(market);
           this.socket.join(market.marketId);
-          this.io.to(market.marketId).emit("getMarketData", market);
+          this.io.to(market.marketId).emit("getMarketData", marketData);
         });
       }
 
@@ -52,11 +54,11 @@ class OddSocket {
       .run(await rethink, (err, cursor) => {
         cursor.each((err, market) => {
           if (err) console.log(err);
-
+          const marketData = this.convertDataToMarket(market.new_val);
           if (market && market.new_val) {
             this.io
               .to(market.new_val.marketId)
-              .emit("getMarketData", market.new_val);
+              .emit("getMarketData", marketData);
           }
         });
       });
@@ -75,6 +77,33 @@ class OddSocket {
           }
         });
       });
+  }
+
+  convertDataToMarket(marketData: IMarket) {
+    return marketData.runners.map(
+      ({ selectionId, ex, status, ...restRunner }: IRunnerType) => {
+        const backLength = checkOddsLength - ex?.availableToBack?.length! ?? 0;
+        if (backLength) {
+          for (let k = 0; k < backLength; k++) {
+            ex?.availableToBack.push({ price: "-", size: "-" });
+          }
+        }
+
+        const layLength = checkOddsLength - ex?.availableToLay?.length! ?? 0;
+        if (layLength) {
+          for (let k = 0; k < layLength; k++) {
+            ex?.availableToLay.push({ price: "-", size: "-" });
+          }
+        }
+
+        return {
+          ...restRunner,
+          status,
+          back: ex?.availableToBack.reverse(),
+          lay: ex?.availableToLay,
+        };
+      }
+    );
   }
 }
 export default OddSocket;
