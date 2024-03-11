8e9a8564dc0b0c5627e06d476f78b76db8541a07
Pj <pj@gmail.com>
2024-03-11 17:29:02 +0530

    Fixed fancy string to float

--- src/controllers/OddsController.ts
+++ src/controllers/OddsController.ts
@@ -98,8 +98,16 @@ export default class OddsController {
                   ballsess: +fancy.ballsess,
                   LayPrice1: parseFloat(fancy.LayPrice1),
                   LaySize1: parseFloat(fancy.LaySize1),
+                  LayPrice2: parseFloat(fancy.LayPrice2),
+                  LaySize2: parseFloat(fancy.LaySize2),
+                  LayPrice3: parseFloat(fancy.LayPrice3),
+                  LaySize3: parseFloat(fancy.LaySize3),
                   BackSize1: parseFloat(fancy.BackSize1),
                   BackPrice1: parseFloat(fancy.BackPrice1),
+                  BackSize2: parseFloat(fancy.BackSize2),
+                  BackPrice2: parseFloat(fancy.BackPrice2),
+                  BackSize3: parseFloat(fancy.BackSize3),
+                  BackPrice3: parseFloat(fancy.BackPrice3),
                 };
                 const fancyRedis = await fancyRepository.fetch(
                   `${matchId}-${fancy.SelectionId}`
@@ -114,8 +122,16 @@ export default class OddsController {
                       ...fancyRedis,
                       LayPrice1: parseFloat(fancyRedis.LayPrice1 as string),
                       LaySize1: parseFloat(fancyRedis.LaySize1 as string),
+                      LayPrice2: parseFloat(fancyRedis.LayPrice2 as string),
+                      LaySize2: parseFloat(fancyRedis.LaySize2 as string),
+                      LayPrice3: parseFloat(fancyRedis.LayPrice3 as string),
+                      LaySize3: parseFloat(fancyRedis.LaySize3 as string),
                       BackPrice1: parseFloat(fancyRedis.BackPrice1 as string),
                       BackSize1: parseFloat(fancyRedis.BackSize1 as string),
+                      BackPrice2: parseFloat(fancyRedis.BackPrice2 as string),
+                      BackSize2: parseFloat(fancyRedis.BackSize2 as string),
+                      BackPrice3: parseFloat(fancyRedis.BackPrice3 as string),
+                      BackSize3: parseFloat(fancyRedis.BackSize3 as string),
                     },
                     { ...fancy, matchId }
                   )
