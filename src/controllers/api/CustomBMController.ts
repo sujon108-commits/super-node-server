bc942f6901a76b09a83907e84a6864f4dd587942
Prashant <prashant@Prashants-MacBook-Pro-2.local>
2024-10-03 23:48:25 +0530

    Added redis for super node

--- src/controllers/api/CustomBMController.ts
+++ src/controllers/api/CustomBMController.ts
@@ -1,18 +1,18 @@
 import { Request, Response } from "express";
-import { redisReplica } from "../../database/redis";
+import { redisSuperNode } from "../../database/redis";
 
 class CustomBMController {
   public static async saveBM(req: Request, res: Response): Promise<Response> {
     try {
       const obj = { ...req.body };
-      await redisReplica.set(
+      await redisSuperNode.set(
         `odds-market-${obj.marketId}`,
         JSON.stringify(obj),
         {
           EX: 14400, // save for 4 hour
         }
       );
-      await redisReplica.publish("getMarketData", JSON.stringify({ ...obj }));
+      await redisSuperNode.publish("getMarketData", JSON.stringify({ ...obj }));
 
       return res.json({ success: true, message: "Saved BM" });
     } catch (e: Error | any) {
